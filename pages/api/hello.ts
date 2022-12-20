bdda37cf752bbb8cbac874d5db9573d6b7429699
Ahmad_Muzaki_SA <ahmadmuzaki.s.a@gmail.com>
2022-12-20 15:12:14 +0700

    update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,5 +6,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe', class:"12 E" })
+  res.status(200).json({ name: 'John Doe', class:"12 B" })
 }
